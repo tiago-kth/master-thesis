29c0e74eb4bb318f8e5ef493f2330d9c2f643806
tiagombp <tiagombp@gmail.com>
2024-03-17 15:47:32 +0100

    adds performance calculations

--- budget-idea/prototypes/soft-body/script.js
+++ budget-idea/prototypes/soft-body/script.js
@@ -42,7 +42,14 @@ function clearCanvas() {
 let anim;
 let t_ant = 0;
 
+let t_perf0, t_perf1;
+let dt_perfs = Array(100)
+
 function loop(t) {
+
+    //perf
+    t_perf0 = window.performance.now();
+    // end perf
 
     if (!t_ant) t_ant = t;
 
@@ -62,7 +69,7 @@ function loop(t) {
 
         // update grid information, if the particle changed cell
         if ( p.getChangedCell() ) {
-            console.log('Particula ', p.index, ' mudou de célula');
+            //console.log('Particula ', p.index, ' mudou de célula');
             grid.removeParticle(p);
             grid.addParticle(p);
         } else {
@@ -75,11 +82,24 @@ function loop(t) {
         p.displayVel(ctx);
     })
     count++
-    ///*if (count < 100)*/ anim = window.requestAnimationFrame(loop);
+
+    // perf
+    t_perf1 = window.performance.now();
+    const dt = t_perf1 - t_perf0;
+    dt_perfs.push(dt);
+    if (dt_perfs.length > 100) {
+        dt_perfs.splice(0, 1);
+        const avg_dt_perf = dt_perfs.reduce( (p, c) => (p + c) ) / dt_perfs.length;
+        console.log("Average time to execute frame: ", avg_dt_perf.toFixed(2), "ms");
+    }
+    // end perf
+
+
+    /*if (count < 100)*/ anim = window.requestAnimationFrame(loop);
 }
 
-//anim = window.requestAnimationFrame(loop);
-loop();
+anim = window.requestAnimationFrame(loop);
+//loop();
 
 // interac
 
