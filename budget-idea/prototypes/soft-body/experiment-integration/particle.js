ccdd5974997b9d7828227669bd2a954f78dd98db
tiagombp <tiagombp@gmail.com>
2024-03-23 14:46:05 +0100

    start experiment with integration methods

---
+++ budget-idea/prototypes/soft-body/experiment-integration/particle.js
@@ -0,0 +1,306 @@
+class Particle {
+
+    index;
+
+    pos;
+    vel;
+    acc;
+    rad;
+    mass;
+    hits;
+
+    grid;
+    cell_col;
+    cell_row;
+    changed_cell = false;
+    cell_previous_col;
+    cell_previous_row;
+
+    constructor(pos, rad, grid, index, mass, vel, acc) {
+        this.index = index;
+        this.grid = grid;
+        this.hits = 0;
+        this.pos = pos;
+        this.rad = rad;
+        this.mass = mass;
+        this.vel = vel;//index == 0 ? new Vec( 2, 1) : new Vec( 0, 0 );
+        this.acc = acc;//new Vec( 0, .02); 
+    }
+
+    applyAcc(array_of_accelerations) {
+        // to-do
+        // sum all acceleration vectors to get a resultant, and assign this vector to this.acc
+    }
+
+    addForce(f) {
+
+        if (this.index == 24) console.log(this.vel.mod(), f);
+        const a = Vec.mult(f, 1/this.mass);
+        //this.acc.selfAdd(a);
+        this.vel.selfAdd(a);
+
+    }
+
+    limitSpeed() {
+        const speed = this.vel.mod();
+        if (speed > params.SPEEDLIMIT) {
+            this.vel.selfMult(params.SPEEDLIMIT / speed)
+        }
+    }
+
+    update(dT) {
+        // v = v0 + a.t
+        this.vel.selfAdd(Vec.mult(this.acc, dT / params.TIMESTEP));
+        // s = s0 + v.t
+        this.pos.selfAdd(Vec.mult(this.vel, dT / params.TIMESTEP));
+        //this.updateGridPos();
+        this.limitSpeed();
+
+    }
+
+    checkBounds2() {
+
+        if (this.pos.x - this.rad < 0) {
+            this.pos.x = this.rad; // Prevent from leaving the canvas from the left side
+            //this.velocity.x *= -1;
+            this.addForce(new Vec(1, 0))
+          } else if (this.pos.x + this.rad > W) {
+            this.pos.x = W - this.rad; // Prevent from leaving the canvas from the right side
+            //this.velocity.x *= -1;
+            this.addForce(new Vec(-1, 0))
+          }
+      
+          if (this.pos.y - this.radius < 0) {
+            this.pos.y = this.rad; // Prevent from leaving the canvas from the top
+            //this.velocity.y *= -1;
+            this.addForce(new Vec(0, 1))
+          } else if (this.pos.y + this.rad > H) {
+            this.pos.y = H - this.rad; // Prevent from leaving the canvas from the bottom
+            //this.velocity.y *= -1;
+            this.addForce(new Vec(0, -1))
+          }
+
+
+    }
+
+
+    checkBounds() {
+        //let opa = false;
+        if ( (this.pos.x + this.rad) > W ) {
+            this.pos.x = W - this.rad;
+            this.vel.x *= -1;
+            //opa = true;
+        } else if ( (this.pos.x - this.rad) < 0 ) {
+            this.pos.x = this.rad;
+            this.vel.x *= -1;
+            //opa = true;
+        }
+
+        if ( (this.pos.y + this.rad) > H ) {
+            this.pos.y = H - this.rad;
+            this.vel.y *= -1;
+            //opa = true;
+        } else if ( (this.pos.y - this.rad) < 0 ) {
+            this.pos.y = this.rad;
+            this.vel.y *= -1;
+            //opa = true;
+        }
+
+        //if (opa) console.log("Opa!");
+    }
+
+    getDifferenceVecFrom(particle) {
+
+        // the basis for the direction and the distance
+        return Vec.sub(this.pos - particle.pos);
+
+    }
+
+    getDistanceFrom(particle) {
+
+        const difference_vector = this.getDifferenceVecFrom(particle);
+        return difference_vector.mod();
+
+    }
+
+    getDirectionFrom(particle) {
+
+        const difference_vector = this.getDifferenceVecFrom(particle);
+        return difference_vector.getUnitDir();
+
+    }
+
+    checkCollisions(particles) {
+
+        let count = 1;
+
+        particles.forEach(that => {
+
+            if (this != that) {
+
+                const difference_vector = Vec.sub(this.pos, that.pos);//this.getDifferenceVecFrom(that);
+
+                const distance = difference_vector.mod()
+                const min_distance = this.rad + that.rad;
+
+                if (distance <= min_distance) {
+
+                    //count++
+
+                    //this.hits++
+                    //that.hits++
+
+                    if (min_distance - distance >= that.rad) console.log("engoliu", this, that);
+
+                    const x1 = this.pos;
+                    const x2 = that.pos;
+                    const x1_x2u = Vec.sub(x1, x2).getUnitDir();
+                    const x2_x1u = Vec.sub(x2, x1).getUnitDir();
+
+                    const v1 = this.vel;
+                    const v2 = that.vel;
+                    const v1_v2 = Vec.sub(v1, v2);
+                    const v2_v1 = Vec.sub(v2, v1);
+
+                    const v1_v2_proj_x1_x2 = Vec.proj(v1_v2, x1_x2u);
+                    const v2_v1_proj_x2_x1 = Vec.proj(v2_v1, x2_x1u);
+
+                    const delta_v1 = Vec.mult(v1_v2_proj_x1_x2, 2 * that.mass / (this.mass + that.mass));
+                    const delta_v2 = Vec.mult(v2_v1_proj_x2_x1, 2 * this.mass / (this.mass + that.mass));
+
+                    //console.log(v1, v2, distance, min_distance, distance - min_distance, v1_v2_proj_x1_x2, v2_v1_proj_x2_x1, delta_v1, delta_v2);
+
+                    this.vel.selfSub(delta_v1);
+                    that.vel.selfSub(delta_v2);
+
+                    const repulsion = Vec.mult(x2_x1u, min_distance - distance);
+          
+                    // Apply repulsion force
+                    this.pos.selfSub(Vec.mult(repulsion, 1/2));
+                    that.pos.selfAdd(Vec.mult(repulsion, 1/2));
+
+                    //console.log(v1, v2);
+
+
+
+                    //const this_repulsion = new Vec( repulsion.x, repulsion.y );
+                    //this_repulsion.mult( 1 / this.rad);
+
+                    //const that_repulsion = new Vec( repulsion.x, repulsion.y );
+                    //that_repulsion.mult( 1 / that.rad);
+
+                    //this.pos.sub(this_repulsion);
+                    //that.pos.add(that_repulsion);
+
+                    
+                    
+
+                }
+            }
+
+        })
+
+    }
+
+    updateGridPos() {
+
+        let col = Math.floor( this.pos.x / this.grid.cell_size);
+        let row = Math.floor( this.pos.y / this.grid.cell_size);
+
+        //console.log(this.index, col, row);
+
+        if ( (col == this.cell_col) & (row == this.cell_row) ) {
+
+            this.changed_cell = false;
+
+        } else {
+
+            // saving the last cell indices to remove the particle from the proper cell -- the previous one;
+            this.cell_previous_row = this.cell_row;
+            this.cell_previous_col = this.cell_col;
+
+            this.cell_col = col;
+            this.cell_row = row;
+
+            this.changed_cell = true;
+
+            //this.grid.removeParticle(this);
+            //this.grid.addParticle(this);
+
+        }
+
+    }
+
+    getPreviousGridPos() {
+        return [this.cell_previous_col, this.cell_previous_row];
+
+    }
+
+    getGridPos() {
+        return [this.cell_col, this.cell_row];
+    }
+
+    getChangedCell() {
+        return this.changed_cell;
+    }
+
+    display(ctx, highlight = false) {
+
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.rad, 0, Math.PI * 2);
+
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "goldenrod";
+        ctx.fillStyle = "khaki";
+        //let red;
+        //if (this.hits > 255) red = 255;
+        //else red = Math.floor(this.hits);
+        //console.log(red);
+        ctx.fill();
+        if (highlight) {
+            ctx.lineWidth = 6;
+            ctx.strokeStyle = "hotpink";
+            ctx.fillStyle = "hotpink";
+        } else {
+
+            //ctx.fillStyle = `rgb(${255} ${255-red} ${255-red})`;
+        }
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+        ctx.font = "14px Serif";
+        ctx.fillStyle = "black";
+        ctx.fillText(this.index, this.pos.x, this.pos.y);
+    }
+
+    displayVel(ctx) {
+
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = "tomato";
+        ctx.lineWidth = 3;
+        ctx.moveTo(this.pos.x, this.pos.y);
+        ctx.lineTo(this.pos.x + this.vel.x * 2, this.pos.y + this.vel.y * 2);
+        ctx.stroke();
+        ctx.restore();
+
+    }
+
+    displayGridCell(ctx) {
+
+        const l = this.grid.cell_size;
+        const x = l * this.cell_col;
+        const y = l * this.cell_row;
+
+        //console.log(this.cell_col, this.cell_row, this.changed_cell);
+
+        ctx.save();
+        ctx.fillStyle = "lightyellow";
+        
+        ctx.fillRect(x, y, l, l);
+        ctx.restore();
+    
+    }
+
+}
---
+++ budget-idea/prototypes/soft-body/experiment-integration/vec.js
@@ -0,0 +1,203 @@
+class Vec {
+
+    x;
+    y;
+    angle;
+    mag;
+
+    constructor(x, y) {
+
+        this.x = x;
+        this.y = y;
+        this.angle = this.#getAngle();
+        this.mag = this.mod();
+
+    }
+
+    #getAngle() {
+
+        const angle = Math.atan( Math.abs(this.y) / Math.abs(this.x) );
+
+        if (this.y == 0) {
+            if (this.x >= 0) return 0;
+            else return Math.PI;
+        } else {
+            if (this.x == 0) {
+                if (this.y >= 0) return Math.PI/2;
+                else return Math.PI * 3/2
+            }
+        }
+
+        let mult = 0; // because for canvas 0 degrees is the y axis pointing up
+        
+        if (this.x < 0 & this.y > 0) mult = 1;
+        if (this.x < 0 & this.y < 0) mult = 2;
+        if (this.x > 0 & this.y < 0) mult = 3;
+        //if (this.x > 0 & this.y > 0) mult = 3;
+
+        // to test: [new Vec(1,1), new Vec(-1,1), new Vec(-1,-1), new Vec(1,-1)].forEach(v => console.log(v.angle * 180 / Math.PI))
+
+        return angle + mult * Math.PI/2;
+
+    }
+
+    mod() {
+
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) )
+
+    }
+
+    selfAdd(vec_b) {
+
+        this.x += vec_b.x;
+        this.y += vec_b.y;
+
+    }
+
+    selfSub(vec_b) {
+
+        this.x -= vec_b.x;
+        this.y -= vec_b.y;
+
+    }
+
+    selfMult(scalar) {
+
+        this.x *= scalar;
+        this.y *= scalar;
+
+    }
+
+    getDifferenceVec(vec_b) {
+
+        const x = this.x - vec_b.x;
+        const y = this.y - vec_b.y;
+
+        return new Vec(x, y);
+
+    }
+
+    getUnitDir() {
+
+        return new Vec(this.x / this.mag, this.y / this.mag);
+
+    }
+
+    static add(vec_a, vec_b) {
+
+        return new Vec(vec_a.x + vec_b.x, vec_a.y + vec_b.y);
+
+    }
+
+    static sub(vec_a, vec_b) {
+
+        return new Vec(vec_a.x - vec_b.x, vec_a.y - vec_b.y);
+
+    }
+
+    static mult(vec, scalar) {
+
+        return new Vec(vec.x * scalar, vec.y * scalar);
+
+    }
+
+    static dot(vec_a, vec_b) {
+
+        return (vec_a.x * vec_b.x) + (vec_a.y * vec_b.y);
+
+    }
+
+    static dist(vec_a, vec_b) {
+
+        return Vec.sub(vec_a, vec_b).mod();
+
+    }
+
+    static proj(vec_a, vec_b) {
+
+        const vec_bU = vec_b.getUnitDir();
+
+        const dot_product = Vec.dot(vec_a, vec_b);
+        
+        return Vec.mult(vec_bU, dot_product / vec_b.mag);
+
+    }
+
+    display(ctx, p0, color) {
+
+        const h = ctx.canvas.height;
+        const w = ctx.canvas.width;
+
+        if (!p0) {
+
+            p0 = new Vec(w/2, h/2);
+
+        } 
+        
+        ctx.save();
+        ctx.strokeStyle = color ? color : "hotpink";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.translate(p0.x, p0.y);
+        ctx.rotate(-1 * this.angle);
+        ctx.moveTo(0,0);
+        ctx.lineTo(this.mag, 0);
+        ctx.translate(this.mag, 0);
+        ctx.rotate(-Math.PI * 5/6);
+        ctx.lineTo(10,0);
+        ctx.moveTo(0,0);
+        ctx.rotate(Math.PI*10/6);
+        ctx.lineTo(10,0);
+        ctx.stroke();
+        ctx.restore();
+
+        /* test
+        const vs = [new Vec(100,100), new Vec(-100,100), new Vec(-100,-100), new Vec(100,-100)]
+        vs.forEach(v => v.display(ctx, new Vec(W/2, H/2)))
+
+        const p_0 = new Vec(W/2, H/2);
+        const v1 = new Vec(100,0);
+        const v2 = new Vec(50,70);
+        const v3 = Vec.proj(v2, v1);
+        v1.display(ctx, p_0, "blue");
+        v2.display(ctx, p_0, "white");
+        v3.display(ctx, p_0, "gold");
+
+        */
+
+    }
+
+    display_as_point() {
+        ctx.save();
+        ctx.strokeStyle = "tomato";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.restore();
+    }
+
+    draw(canvas) {
+
+        const ctx = canvas.getContext('2d');
+
+        const x0 = canvas.W / 2;
+        const y0 = canvas.H / 2;
+
+        
+
+
+
+    }
+
+    /* this make it possible to use the utility function without instantiating an object */
+    static fromAngle(mag, ang, p0) {
+
+        let x = p0.x + Math.cos(ang) * mag;
+        let y = p0.y - Math.sin(ang) * mag;
+
+        return new Vec(x, y)
+
+    }
+
+}
